5c333255bf329aa74cc4cb3679a5992da6d2d5a5
rptkm <palatkarares@gmail.com>
2025-12-24 12:08:41 +0200

    fin, without grid

--- js/script.js
+++ js/script.js
@@ -1,26 +1,32 @@
 // Wait for DOM to load
 document.addEventListener('DOMContentLoaded', () => {
-
+    let amount = 0
     function randomizeRGB() {
         const randomNumber = Math.floor(Math.random() * 16777215)
         const hexColor = "#" + randomNumber.toString(16).padStart(6, "0")
 
         return hexColor
     }
+    while(amount <=0 || amount > 100){
+      amount = prompt("How many squares you want? min 1 - max 100",16)
+    }
+    let size = 100/amount
+    let cols = Math.floor(Math.sqrt(amount))
+    let restCols = amount - cols*cols
 
     function createDivs(num) {
         for (let i = 0; i < num; i++) {
             const newDiv = document.createElement("div")
-            newDiv.id = `div${i}`
-            console.log(newDiv.id)
             newDiv.className = "divs"
-            newDiv.style.height = "50px"
-            newDiv.style.width = "50px"
-            newDiv.innerHTML = "."
+            newDiv.style.height = 0
+            newDiv.style.paddingBottom = `${size}%`
+            newDiv.style.width = size + "%"
+            newDiv.innerHTML = ""
             newDiv.style.backgroundColor = randomizeRGB()
             document.getElementById("divContainer").appendChild(newDiv)
         }
     }
+    createDivs(amount)
 
 
     document.querySelectorAll(".divs").forEach(divs => {
@@ -35,8 +41,5 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     })
 
-    const numOfDivs = document.getElementById("inputDivs")
-    console.log(numOfDivs.value)
-    numOfDivs.addEventListener("change", createDivs(numOfDivs.value))
 
 });
